5e332f520c658ac7d7c277f5d4a85f77e2a76461
Nikolay1990 <Nikolaicev1990@gmail.com>
2023-11-07 22:07:22 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# CI-test
+
+[![Build status](https://ci.appveyor.com/api/projects/status/ql0rsrws9h51bhvr?svg=true)](https://ci.appveyor.com/project/Nikolaytcev/orp-js-homework)
---
+++ src/js/__test__/test.js
@@ -0,0 +1,11 @@
+import { orderByProps } from "../sort";
+
+test("testing sort function", () => {
+    expect(orderByProps({name: 'мечник', health: 10, level: 2, attack: 80, defence: 40}, ["name", "level"])).toEqual([
+        {key: "name", value: "мечник"},
+        {key: "level", value: 2},
+        {key: "attack", value: 80}, 
+        {key: "defence", value: 40},
+        {key: "health", value: 10}
+      ])
+})
